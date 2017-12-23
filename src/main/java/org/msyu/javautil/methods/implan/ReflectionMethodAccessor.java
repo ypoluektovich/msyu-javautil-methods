4711954b3068d6b12ac673ed65bf36636408596c
Yanus Poluektovich <ypoluektovich@gmail.com>
2017-12-24 02:57:15 +0300

    add invokeDirectly() for use with default methods

--- src/main/java/org/msyu/javautil/methods/implan/ReflectionMethodAccessor.java
+++ src/main/java/org/msyu/javautil/methods/implan/ReflectionMethodAccessor.java
@@ -2,9 +2,30 @@ package org.msyu.javautil.methods.implan;
 
 import org.msyu.javautil.methods.MethodSignature;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public final class ReflectionMethodAccessor implements MethodAccessor {
+
+	private static final Constructor<MethodHandles.Lookup> LOOKUP_CONSTRUCTOR;
+
+	static {
+		try {
+			LOOKUP_CONSTRUCTOR = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class);
+			if (!LOOKUP_CONSTRUCTOR.isAccessible()) {
+				LOOKUP_CONSTRUCTOR.setAccessible(true);
+			}
+		} catch (Exception e) {
+			throw mhFailure(e);
+		}
+	}
+
+	private static RuntimeException mhFailure(Throwable cause) {
+		return new RuntimeException("MethodHandle failure", cause);
+	}
 
 	private final Method method;
 
@@ -20,6 +41,33 @@ public final class ReflectionMethodAccessor implements MethodAccessor {
 	@Override
 	public boolean hasImplementation() {
 		return method.isDefault();
+	}
+
+	/**
+	 * Delegates to the underlying {@link Method#invoke(Object, Object...)}.
+	 */
+	public Object invokeViaReflection(Object target, Object... args) throws InvocationTargetException, IllegalAccessException {
+		return method.invoke(target, args);
+	}
+
+	/**
+	 * Invokes the represented method without virtual dispatch.
+	 */
+	public Object invokeDirectly(Object target, Object... args) throws InvocationTargetException {
+		// todo: optimize and/or expose facilities for user-side optimizations
+		MethodHandles.Lookup lookup;
+		MethodHandle getter;
+		try {
+			lookup = LOOKUP_CONSTRUCTOR.newInstance(method.getDeclaringClass());
+			getter = lookup.unreflectSpecial(method, method.getDeclaringClass());
+		} catch (Exception e) {
+			throw mhFailure(e);
+		}
+		try {
+			return getter.bindTo(target).invokeWithArguments(args);
+		} catch (Throwable throwable) {
+			throw new InvocationTargetException(throwable);
+		}
 	}
 
 }
---
+++ src/test/java/org/msyu/javautil/methods/implan/example/BasicExample.java
@@ -0,0 +1,61 @@
+package org.msyu.javautil.methods.implan.example;
+
+import org.msyu.javautil.methods.MethodSignature;
+import org.msyu.javautil.methods.implan.InterfacePlan;
+import org.msyu.javautil.methods.implan.MethodPlan;
+import org.msyu.javautil.methods.implan.ReflectionMethodAccessor;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Proxy;
+
+import static java.util.Collections.singletonList;
+import static org.msyu.javautil.methods.implan.ImplementationPlanBuilder.planFor;
+import static org.msyu.javautil.methods.implan.ReflectionInterfaceAccessor.reflect;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertThrows;
+import static org.testng.Assert.expectThrows;
+
+public class BasicExample {
+
+	public interface I {
+		String a(Object o);
+		default int b(Object o) {
+			return System.identityHashCode(o);
+		}
+		Object c();
+		default void d() {
+			throw new RuntimeException("dead");
+		}
+	}
+
+	@Test
+	public void test() {
+		InterfacePlan<ReflectionMethodAccessor> ifacePlan = planFor(reflect(singletonList(I.class)));
+		I impl = (I) Proxy.newProxyInstance(
+				BasicExample.class.getClassLoader(),
+				new Class[]{I.class},
+				(proxy, method, args) -> {
+					MethodSignature signature = new MethodSignature(method);
+					MethodPlan<ReflectionMethodAccessor> plan = ifacePlan.getPlanFor(signature);
+					if (plan.hasOneImplementation()) {
+						try {
+							return plan.getTheImplementation().invokeDirectly(proxy, args);
+						} catch (InvocationTargetException e) {
+							throw e.getCause();
+						}
+					} else if (signature.name.equals("a")) {
+						return args[0].toString();
+					} else {
+						throw new UnsupportedOperationException(method.getName());
+					}
+				}
+		);
+		assertEquals(impl.a("string"), "string");
+		assertEquals(impl.b("string"), System.identityHashCode("string"));
+		assertThrows(UnsupportedOperationException.class, impl::c);
+		RuntimeException runtimeException = expectThrows(RuntimeException.class, impl::d);
+		assertEquals(runtimeException.getMessage(), "dead");
+	}
+
+}
